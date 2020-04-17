e9f9f070022ea5c558078c06e6da034015e54fd2
Martin Borho <martin@borho.net>
2020-04-17 17:34:21 +0200

    Add pin support for temporal.

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.14
 
 require (
 	github.com/hashicorp/terraform-plugin-sdk v1.9.0
-	github.com/ipfs/go-cid v0.0.1
+	github.com/ipfs/go-cid v0.0.1 // indirect
 	github.com/ipfs/go-ipfs-api v0.0.3
 	github.com/ipfs/go-ipfs-files v0.0.6
-	github.com/multiformats/go-multihash v0.0.8
+	github.com/multiformats/go-multihash v0.0.8 // indirect
 )
--- resource_pin.go
+++ resource_pin.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -12,6 +13,11 @@ type PinInfo struct {
 
 type PinListResponse struct {
 	Keys map[string]PinInfo `json:"Keys"`
+}
+
+type PinResponse struct {
+	Progress int      `json:"Progress"`
+	Pins     []string `json:"Pins"`
 }
 
 func resourcePin() *schema.Resource {
@@ -34,10 +40,21 @@ func resourcePinCreate(d *schema.ResourceData, m interface{}) error {
 	client := m.(*Client)
 	cid := d.Get("cid").(string)
 
-	err := client.shell.Pin(cid)
+	var pinResp PinResponse
+	req := client.shell.Request("pin/add", cid)
+	req.Option("recursive", true)
+
+	if client.temporalToken != "" {
+		// X-Hold-Time: 10 - months holdtime
+		req.Header("Authorization", fmt.Sprintf("Bearer %s", client.temporalToken))
+	}
+
+	err := req.Exec(context.Background(), &pinResp)
 	if err != nil {
 		return err
 	}
+	_ = pinResp
+
 	d.SetId(cid)
 	return resourcePinRead(d, m)
 }
@@ -46,20 +63,22 @@ func resourcePinRead(d *schema.ResourceData, m interface{}) error {
 	client := m.(*Client)
 	cid := d.Get("cid").(string)
 
-	var resp PinListResponse
-	req := client.shell.Request("pin/ls", cid)
-	req.Option("quit", true)
+	if client.temporalToken == "" {
+		var resp PinListResponse
+		req := client.shell.Request("pin/ls", cid)
+		req.Option("quit", true)
 
-	err := req.Exec(context.Background(), &resp)
-	if err != nil {
+		err := req.Exec(context.Background(), &resp)
+		if err != nil {
+			d.SetId("")
+			return nil
+		}
+		if len(resp.Keys) == 1 && resp.Keys[cid].Type != "indirect" {
+			return nil
+		}
+
 		d.SetId("")
-		return nil
 	}
-	if len(resp.Keys) == 1 && resp.Keys[cid].Type != "indirect" {
-		return nil
-	}
-
-	d.SetId("")
 	return nil
 }
 
@@ -67,9 +86,11 @@ func resourcePinDelete(d *schema.ResourceData, m interface{}) error {
 	client := m.(*Client)
 	cid := d.Get("cid").(string)
 
-	err := client.shell.Unpin(cid)
-	if err != nil {
-		return err
+	if client.temporalToken == "" {
+		err := client.shell.Unpin(cid)
+		if err != nil {
+			return err
+		}
 	}
 
 	d.SetId("")
